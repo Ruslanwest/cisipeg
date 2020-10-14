486073a82727fc701ea864d741d2f8f10d66fe9e
Ruslanwest <ruslan.label2@gmail.com>
2020-10-14 23:21:29 +0700

    commit on master

--- application/views/v_footer.php
+++ application/views/v_footer.php
@@ -3,3 +3,7 @@
 </div>
 
 <script src="<?php echo base_url(); ?>assets/js/jquery-3.2.1.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/propper.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+</body>
+</html>
