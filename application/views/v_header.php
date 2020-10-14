d482bc0284879e500f15144177a620316ab4c25c
Ruslanwest <ruslan.label2@gmail.com>
2020-10-14 23:04:44 +0700

    commit on master

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -1,3 +1,59 @@
-<div class="container">
-    <h2>Aplikasi Kepegawaian</h2>
-</div>
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+					data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Cisipeg</a>
+			</div>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home<span class="sr-only">(current)</span></a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+							aria-expanded="false">Referensi <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Kode Gender</a></li>
+							<li><a href="#">Kode Level Pendidikan</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+							aria-expanded="false">Data Pegawai <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Data Pribadi</a></li>
+							<li><a href="#">Data Anak Pegawai</a></li>
+							<li><a href="#">Data Riwayat Pendidikan</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="">Entri Kehadiran</a>
+					</li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+							aria-expanded="false">Laporan<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Data Pegawai</a></li>
+							<li><a href="#">Data Kehadiran</a></li>
+						</ul>
+                    </li>
+                    <li>
+						<a href="">About</a>
+					</li>
+				</ul>
+				<form class="navbar-form navbar-right" role="search">
+					<div class="form-group">
+						<input type="text" class="form-control" placeholder="Search">
+					</div>
+					<button type="submit" class="btn btn-default">Submit</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</nav>
