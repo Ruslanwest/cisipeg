9b52f5895e34fecaf5fd4cd61de595d9356f9326
Ruslanwest <ruslan.label2@gmail.com>
2020-10-14 23:13:35 +0700

    commit on master

--- application/views/v_footer.php
+++ application/views/v_footer.php
@@ -1,3 +1,5 @@
 <div class="container">
     &copy; 2020 - <?php echo date('Y'); ?> Dikembangan oleh Ruslan Permana
 </div>
+
+<script src="<?php echo base_url(); ?>assets/js/jquery-3.2.1.min.js"></script>
