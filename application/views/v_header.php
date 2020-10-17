6d6b31650df5233b3d672f525fdf67b47e5e2040
Ruslanwest <ruslan.label2@gmail.com>
2020-10-18 01:05:42 +0700

    master

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -14,7 +14,7 @@
 			</div>
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
-					<li class="active"><?=anchor("c_start","Home"); ?></li>
+					<li><?=anchor("c_start","Home"); ?></li>
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
 							aria-expanded="false">Referensi <span class="caret"></span></a>
