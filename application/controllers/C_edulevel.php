aed7b4da3aaa11018c1b9ccdb22681d68144f622
Ruslanwest <ruslan.label2@gmail.com>
2020-10-26 22:09:35 +0700

    commit on developer

--- application/controllers/C_edulevel.php
+++ application/controllers/C_edulevel.php
@@ -4,9 +4,25 @@ class C_edulevel extends CI_Controller {
     //Fungsi untuk Underconst
     public function index(){
         $data=array();
-        $data["controller"]=$this->router->class;
-        $data["method"]=$this->router->method;
+        $data["controller"]=$this->router->fetch_class();
+        $data["method"]=$this->router->fetch_method();
         $this->load->view("v_temp",$data);
+    }
+
+    public function form4add(){
+        $data=array();
+        $this->load->model("m_edulevel");
+        $data=$this->m_edulevel->initdata();
+        $data["myview"]="v_frmedulevel";
+    }
+
+    public function save4add(){
+        $data=array();
+        $this->load->model("m_edulevel");
+        $data=$this->m_edulevel->initdata();
+        $data["saveresult"]=$this->m_edulevel->insertdata($data);
+        $data["myview"]="v_frmgedulevelsaved";
+        $this->load->view("v_main", $data);
     }
 }
 
